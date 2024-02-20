04616d2b9e78ddcc038a3a62773da44ccbffcc32
Nick <133172211+nickmcc90@users.noreply.github.com>
2024-02-20 15:59:36 -0500

    2/20

---
+++ 01-templateLiterals.js
@@ -0,0 +1,11 @@
+let word1 = "Hello";
+let num1 = 8;
+let num2 = 10;
+
+let example = `${word1}
+Mr. ${num1 + num2}.
+`;
+
+console.log(example);
+
+// We learned about this in our javascriptnew1 repository.
