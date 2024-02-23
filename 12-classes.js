f7ca17337e0ea7dc1298e6893810bb545e41bcb7
Nick <133172211+nickmcc90@users.noreply.github.com>
2024-02-22 20:01:37 -0500

    2/22

---
+++ 12-classes.js
@@ -0,0 +1,12 @@
+class Animal {
+  constructor(type, legs) {
+    this.type = type;
+    this.legs = legs;
+  }
+}
+
+let cat = new Animal('Cat', 4);
+
+console.log(cat.legs);
+
+// timestamp: 41:11;
